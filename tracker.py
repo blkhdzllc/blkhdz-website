43c1ad9331870623e7436e855f8cde2087c09251
blkhdzllc <evoviii2004@gmail.com>
2026-02-25 09:23:04 -0500

    Create tracker.py

---
+++ tracker.py
@@ -0,0 +1,31 @@
+import requests
+import json
+import os
+from datetime import datetime
+
+# Set numbers you want to track
+watch_list = ["75192", "75337", "10316"]
+API_KEY = os.getenv('REBRICKABLE_KEY')
+
+def get_lego_data():
+    results = []
+    for set_id in watch_list:
+        url = f"https://rebrickable.com/api/v3/lego/sets/{set_id}-1/"
+        response = requests.get(url, headers={'Authorization': f'key {API_KEY}'})
+        
+        if response.status_code == 200:
+            data = response.json()
+            results.append({
+                "set_id": set_id,
+                "name": data['name'],
+                "year": data['year'],
+                "parts": data['num_parts'],
+                "img": data['set_img_url'],
+                "last_updated": datetime.now().strftime("%Y-%m-%d")
+            })
+    
+    with open('data.json', 'w') as f:
+        json.dump(results, f, indent=4)
+
+if __name__ == "__main__":
+    get_lego_data()
