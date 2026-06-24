1fb90174ed4d0e9400d940744e6dd7c5e41e7e94
blkhdzllc <blkhdz@outlook.com>
2026-06-24 15:33:55 -0400

    Update sync_unified.py

--- sync_unified.py
+++ sync_unified.py
@@ -1,21 +1,26 @@
 import json
 import os
 import sys
-
 sys.path.append(os.getcwd())
 from services.ebay_client import get_active_ebay_inventory
 
 def assign_tags(title):
     t = title.lower()
-    if any(kw in t for kw in ['lego', 'minifig', 'brickheadz', 'polybag', 'star wars', 'ninjago', 'city']): 
-        return ['lego']
-    if any(kw in t for kw in ['diecast', 'pop race', 'hot wheels', 'mini gt', 'tarmac', 'spark']): 
-        return ['diecast']
-    return ['other']
+    tags = ['all']
+    
+    if any(kw in t for kw in ['lego', 'minifig', 'brickheadz', 'polybag', 'star wars', 'ninjago', 'city', 'technic', 'creator', 'icons']): 
+        tags.append('lego')
+    if any(kw in t for kw in ['diecast', 'pop race', 'hot wheels', 'mini gt', 'tarmac', 'spark', 'looksmart', '1/64', '1/43']): 
+        tags.append('diecast')
+    if any(kw in t for kw in ['pc', 'gaming', 'electronics', 'gpu', 'motherboard', 'nintendo', 'sega']):
+        tags.append('electronics')
+        
+    return list(set(tags))
 
 def sync_inventory():
     raw_items = get_active_ebay_inventory()
     processed = []
+    
     for item in raw_items:
         item['tags'] = assign_tags(item.get('title', ''))
         processed.append(item)
